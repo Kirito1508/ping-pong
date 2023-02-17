3c22b4dd7d3c53894defecde6a55c41b3bcc220f
Andrey <125375998+Kirito1508@users.noreply.github.com>
2023-02-17 20:01:36 +0300

    Add files via upload
    player 2 is a computer

--- ping-pong.py
+++ ping-pong.py
@@ -16,18 +16,26 @@ class GameSprite(sprite.Sprite):
 class Player(GameSprite):
     def update_l(self):
         keyp=key.get_pressed()
-        if keyp[K_w] and self.rect.y<win_h-80:
-            self.rect.y+=self.speed
-        if keyp[K_s] and self.rect.y>5:
+        if keyp[K_w] and self.rect.y>5:
             self.rect.y-=self.speed
+        if keyp[K_s] and self.rect.y<win_h-80:
+            self.rect.y+=self.speed
     def update_r(self):
-        keyp=key.get_pressed()
-        if keyp[K_UP] and self.rect.y<win_h-80:
+        if self.rect.y<katon_koboku_no_jutsu.rect.y:
             self.rect.y+=self.speed
-        if keyp[K_DOWN] and self.rect.y>5:
+        elif self.rect.y>katon_koboku_no_jutsu.rect.y:
             self.rect.y-=self.speed
+        # keyp=key.get_pressed()
+        # if keyp[K_UP] and self.rect.y>5:
+        #     self.rect.y-=self.speed
+        # if keyp[K_DOWN] and self.rect.y<win_h-80:
+        #     self.rect.y+=self.speed
 game_over=True
 finish=False
+font.init()
+font=font.Font(None,35)
+pl1=font.render('Игрок 1 проиграл',True,(72, 209, 204))
+pl2=font.render('Игрок 2 проиграл',True,(72, 209, 204))
 win_w=1280
 win_h=720
 speed_y=3
@@ -35,9 +43,9 @@ speed_x=3
 background = transform.scale(image.load("pole_dla ping ponga.jpg"), (win_w, win_h))
 win=display.set_mode((win_w,win_h))
 clock=time.Clock()
-player1=Player('left.png',30,200,30,130,150)
-player2=Player('right.png',1220,200,30,130,150)
-katon_koboku_no_jutsu=GameSprite('ball.png',615,330,50,50,50)
+player1=Player('left.png',30,200,30,130,2)
+player2=Player('right.png',1220,200,30,130,2)
+katon_koboku_no_jutsu=GameSprite('ball.png',615,330,50,50,80)
 
 while game_over:
     for e in event.get():
@@ -47,8 +55,23 @@ while game_over:
         win.blit(background,(0,0))
         player1.update_l()
         player2.update_r()
+        katon_koboku_no_jutsu.rect.x+=speed_x
+        katon_koboku_no_jutsu.rect.y+=speed_y
+        if sprite.collide_rect(player1,katon_koboku_no_jutsu)or sprite.collide_rect(player2,katon_koboku_no_jutsu):
+            speed_x*=-1
+        if katon_koboku_no_jutsu.rect.y>win_h-50 or katon_koboku_no_jutsu.rect.y<0:
+            speed_y*=-1
+        if katon_koboku_no_jutsu.rect.x<0:
+            finish=True
+            win.blit(pl1,(615,330))
+            game_over=True
+        if katon_koboku_no_jutsu.rect.x>win_w:
+            finish=True
+            win.blit(pl2,(615,330))
+            game_over=True
         player1.reset()
         player2.reset()
         katon_koboku_no_jutsu.reset()
+    
     display.update()
     clock.tick(60)
